7cb763ba73a53a7ce3b455f122e3616184e952b8
Vishesh Handa <me@vhanda.in>
2022-05-01 14:04:21 +0200

    Make it a proper CLI app

--- go.mod
+++ go.mod
@@ -4,18 +4,21 @@ go 1.18
 
 require (
 	github.com/otiai10/copy v1.7.0
+	github.com/urfave/cli/v2 v2.5.1
 	github.com/ztrue/tracerr v0.3.0
 	gopkg.in/src-d/go-git.v4 v4.13.1
 	gotest.tools/v3 v3.2.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd // indirect
 	github.com/logrusorgru/aurora v0.0.0-20181002194514-a7b3b318ed4e // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/src-d/gcfg v1.4.0 // indirect
 	github.com/xanzy/ssh-agent v0.2.1 // indirect
--- main.go
+++ main.go
@@ -2,26 +2,49 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"os"
 
+	cli "github.com/urfave/cli/v2"
 	"github.com/ztrue/tracerr"
 )
 
 func main() {
-	fmt.Println("Hello")
+	app := &cli.App{
+		Name:  "git-auto-sync",
+		Usage: "fight the loneliness!",
+		Action: func(c *cli.Context) error {
+			cwd, err := os.Getwd()
+			if err != nil {
+				return tracerr.Wrap(err)
+			}
+
+			err = autoSync(cwd)
+			if err != nil {
+				return tracerr.Wrap(err)
+			}
+			fmt.Println("Done")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-type Config struct {
-	repoPath     string
-	pollInterval time.Duration
-}
+// type Config struct {
+// 	repoPath     string
+// 	pollInterval time.Duration
+// }
 
 // what remotes?
 // what branches?
 
-func poll() {
-	fmt.Println("Poll")
-}
+// func poll() {
+// 	fmt.Println("Poll")
+// }
 
 func autoSync(repoPath string) error {
 	err := commit(repoPath)
