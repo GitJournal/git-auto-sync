9e65be8419d8034469cce4089625f42c766d4ff8
Vishesh Handa <me@vhanda.in>
2022-05-03 00:05:02 +0200

    Add more tests for rebasing
    Clearly one fails, as it is supposed to be.

--- rebase_test.go
+++ rebase_test.go
@@ -56,6 +56,39 @@ func Test_RebaseRemoteCommits(t *testing.T) {
 	assert.Equal(t, head.Hash(), plumbing.NewHash("ccda8f2e691aa416791a10afc74ccdbd1cb419fe"))
 }
 
-// new commits on both, no conflict
+// New commits on both, no conflict
+func Test_RebaseBothCommitsNoConflict(t *testing.T) {
+	repoPath := PrepareMultiFixtures(t, "rebase_both_commits", []string{"rebase_parent"})
 
-// new commits on both, some kind of conflict
+	err := rebase(repoPath)
+	assert.NilError(t, err)
+
+	r, err := git.PlainOpen(repoPath)
+	assert.NilError(t, err)
+
+	head, err := r.Head()
+	assert.NilError(t, err)
+
+	assert.Check(t, head.Hash() != plumbing.NewHash("ccda8f2e691aa416791a10afc74ccdbd1cb419fe"))
+	assert.Check(t, head.Hash() != plumbing.NewHash("5779561afa9d074ae8d20974861c54757429aca9"))
+	assert.Check(t, head.Hash() != plumbing.NewHash("7fc438e0c9cc4f58178a1efe8521e52f0f8ee688"))
+}
+
+// New commits on both, some kind of conflict
+func Test_RebaseBothCommitsConflict(t *testing.T) {
+	repoPath := PrepareMultiFixtures(t, "rebase_both_commits_conflict", []string{"rebase_parent"})
+
+	err := rebase(repoPath)
+	assert.NilError(t, err)
+
+	r, err := git.PlainOpen(repoPath)
+	assert.NilError(t, err)
+
+	head, err := r.Head()
+	assert.NilError(t, err)
+
+	assert.Check(t, head.Hash() != plumbing.NewHash("ccda8f2e691aa416791a10afc74ccdbd1cb419fe"))
+	assert.Check(t, head.Hash() != plumbing.NewHash("5779561afa9d074ae8d20974861c54757429aca9"))
+	assert.Check(t, head.Hash() != plumbing.NewHash("7fc438e0c9cc4f58178a1efe8521e52f0f8ee688"))
+	assert.Check(t, head.Hash() != plumbing.NewHash("28cc969d97ddb7640f5e1428bbc8f2947d1ffd57"))
+}
